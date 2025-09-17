52a1cf678dbfbb2861b458a0185ed7cdd426d1ea
Murat Cengiz <devmuratcengiz@outlook.com>
2025-09-17 18:48:24 +0300

    Created project files.

--- E-CommerceClient/src/App.jsx
+++ E-CommerceClient/src/App.jsx
@@ -1,14 +1,13 @@
 // App.jsx
 import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
-import Home from "./pages/Home";
-import Products from "./pages/Products";
+import Home from "./features/ui/pages/Home";
+import Products from "./features/ui/pages/Products";
 
 
 
 function App() {
   return (
     <>
-      <div>asd</div>
     </>
   );
 }
