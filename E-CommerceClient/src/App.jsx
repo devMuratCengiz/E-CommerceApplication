a4e68b424dbfb454f1a55292a8053533bb56d439
Murat Cengiz <devmuratcengiz@outlook.com>
2025-09-17 16:54:29 +0300

    Initial-2

--- E-CommerceClient/src/App.jsx
+++ E-CommerceClient/src/App.jsx
@@ -3,26 +3,12 @@ import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-route
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 
-function Navigation() {
-  const navigate = useNavigate(); // hook burada kullanılmalı
 
-  return (
-    <div style={{ display: "flex", gap: "10px", marginBottom: "20px" }}>
-      <button onClick={() => navigate("/Home")}>Home</button>
-      <button onClick={() => navigate("/Products")}>Products</button>
-    </div>
-  );
-}
 
 function App() {
   return (
-    <Router>
-      <Navigation /> {/* Router içinde olduğu için useNavigate çalışıyor */}
-      <Routes>
-        <Route path="/Home" element={<Home />} />
-        <Route path="Products" element={<Products />} />
-      </Routes>
-    </Router>
+    <>
+    </>
   );
 }
 
